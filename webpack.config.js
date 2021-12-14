7e232eacb50338b73b116add0f135381cdcdb69a
radhp2005@gmail.com <radhp2005@gmail.com>
2021-12-13 22:30:14 -0500

    source map configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+}
 module.exports = {
- mode: "development",
- devtool: false,
+ mode: mode,
+ devtool: "source-map",
  module: {
      rules: [
          {
