32f80543c6885135ca59a67fc5c7aab2f40277a0
radhp2005@gmail.com <radhp2005@gmail.com>
2021-12-13 22:11:13 -0500

    Babel setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 module.exports = {
  mode: "development",
  devtool: false,
+ module: {
+     rules: [
+         {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+              loader: "babel-loader",
+          }   
+         }
+     ]
+ },
  devServer: {
     static: "./dist",
     compress: true,
