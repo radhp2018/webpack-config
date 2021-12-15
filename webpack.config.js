7291658fd92428cc5f127d05795a36a64fc54449
radhp2005@gmail.com <radhp2005@gmail.com>
2021-12-15 08:56:25 -0500

    HTML plugins and clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 let target = "web";
 
 let mode = "development";
@@ -11,6 +15,7 @@ module.exports = {
  mode: mode,
  target: target,
  output: {
+    path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash][ext][query]"
  },
  devtool: "source-map",
@@ -45,7 +50,14 @@ module.exports = {
          }
      ]
  },
- plugins: [new MiniCssExtractPlugin()],
+ plugins: [
+    new CleanWebpackPlugin(),
+     new MiniCssExtractPlugin(), 
+     new HtmlWebpackPlugin(
+         {
+         template: "./src/index.html"
+        })
+],
  resolve: {
     extensions: [".js", ".jsx"]
  },
